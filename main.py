f05296745eb99fc166e864ea8dee9dc533995dcb
milamedin <74660419+milamedin@users.noreply.github.com>
2021-02-04 10:50:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ bill = float(input("What was the total bill? $"))
 tip = float(input("What percentage tip would you like to give? 10, 12 or 15? "))/100
 people = int(input("How many people to split the bill: "))
 x = round((bill + bill*tip)/people, 2)
-print(f"Each person should pays ${x}")
+print(f"Each person should pay ${x}")
